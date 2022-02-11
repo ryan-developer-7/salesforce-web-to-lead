458d1237f96144e60b6416831166a6a6b79e6a61
Ryan Wilson <ryan.wilson@capgemini.com>
2022-02-11 15:34:17 +0000

    first commit

--- js/createLead.js
+++ js/createLead.js
@@ -1,12 +1,6 @@
 $( "#submit" ).click(function() {
     $.post( "https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8", 
-    { 
-        oid: "00D4K0000016LVA", 
-        first_name: $( "#first_name" ).value,
-        last_name: $( "#last_name" ).value,
-        phone: $( "#phone" ).value,
-        "00N4K000002A1uc": $( "#00N4K000002A1uc" ).value
-    } 
+    $( "#form" ).serialize()
     
     );
 });
